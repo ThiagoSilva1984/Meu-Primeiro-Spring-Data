b8bdc3aa27102e1604959a8b13c81d3a138a0581
ThiagoSilva1984 <dasilva.thiagopaulo@gmail.com>
2022-09-23 16:41:09 -0300

    Criada classe para Relatorios

--- spring-data/src/main/java/br/com/alura/springdata/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/alura/springdata/SpringDataApplication.java
@@ -6,19 +6,31 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.springdata.orm.Cargo;
 import br.com.alura.springdata.service.CrudCargoService;
+import br.com.alura.springdata.service.CrudFuncionarioService;
+import br.com.alura.springdata.service.CrudUnidadeTrabalhoService;
+import br.com.alura.springdata.service.RelatoriosService;
 
 @SpringBootApplication
 public class SpringDataApplication implements CommandLineRunner {
-	//variavel auxiliar para fechar o programa
-	private Boolean system = true;
 	
 	private final CrudCargoService cargoService;
+	private final CrudFuncionarioService funcionarioService;
+	private final CrudUnidadeTrabalhoService unidadeTrabalhoService;
+	private final RelatoriosService relatoriosService;
+	
+	//variavel auxiliar para fechar o programa
+	private Boolean system = true;
 
 	// esse é o construtor da minha classe SpringDataApplication
-	public SpringDataApplication(CrudCargoService cargoService) {
+	public SpringDataApplication(CrudCargoService cargoService, 
+			CrudFuncionarioService funcionarioService,
+			CrudUnidadeTrabalhoService unidadeTrabalhoService,
+			RelatoriosService relatoriosService) {
 		this.cargoService = cargoService;
+		this.funcionarioService = funcionarioService;
+		this.unidadeTrabalhoService = unidadeTrabalhoService;
+		this.relatoriosService = relatoriosService;
 	}
 
 	public static void main(String[] args) {
@@ -33,11 +45,23 @@ public class SpringDataApplication implements CommandLineRunner {
 			System.out.println("QUAL AÇÃO DESEJA EXECUTAR?");
 			System.out.println("0 - Para sair do programa.");
 			System.out.println("1 - Cargo.");
+			System.out.println("2 - Funcionario.");
+			System.out.println("3 - Unidade de Trabalho.");
+			System.out.println("4 - Relatorios.");
 			
 			int action = sc.nextInt();
 			switch (action) {
 			case 1:
 				cargoService.inicial(sc);
+				break;
+			case 2:
+				funcionarioService.inicial(sc);
+				break;
+			case 3:
+				unidadeTrabalhoService.inicial(sc);
+				break;
+			case 4:
+				relatoriosService.inicial(sc);
 				break;
 			default:
 				system = false;
